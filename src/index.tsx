1b5deeb0212ee2fd4b9c94e7743a5374828877a2
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2022-04-24 12:16:33 +0300

    added React Query

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from "react-query/devtools";
 
 import { Provider } from 'react-redux';
 import { store } from './app/store';
@@ -13,19 +17,24 @@ import { Counter } from './features/counter/Counter';
 
 import './index.css';
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route path="home" element={<Home />} />
-            <Route path="countries" element={<Countries />} />
-            <Route path="counter" element={<Counter />} />
-          </Route>
-        </Routes>
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route path="home" element={<Home />} />
+              <Route path="countries" element={<Countries />} />
+              <Route path="counter" element={<Counter />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen />
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>
 );
