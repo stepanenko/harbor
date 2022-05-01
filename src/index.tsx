373f7c38a5a06f00c2b802faf89289f45d1d3ab2
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2022-05-02 00:09:45 +0300

    added MUI

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import ReactDOM from 'react-dom/client';
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from "react-query/devtools";
 
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
+import blue from '@mui/material/colors/blue';
 
 import App from './App';
 import Home from './features/home/Home';
@@ -18,21 +21,29 @@ import { Counter } from './features/counter/Counter';
 import './index.css';
 
 const queryClient = new QueryClient();
+const theme = createTheme({
+  palette: {
+    primary: blue,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />}>
-              <Route path="home" element={<Home />} />
-              <Route path="countries" element={<Countries />} />
-              <Route path="counter" element={<Counter />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<App />}>
+                <Route path="home" element={<Home />} />
+                <Route path="countries" element={<Countries />} />
+                <Route path="counter" element={<Counter />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
         <ReactQueryDevtools initialIsOpen />
       </QueryClientProvider>
     </Provider>
